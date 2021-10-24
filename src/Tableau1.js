df01fa0c583838127ec043b3a72640e9ffbd69fe
davidmarsprof <72275002+davidmarsprof@users.noreply.github.com>
2021-10-24 16:06:26 +0200

    inversion scroll

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -177,10 +177,10 @@ class Tableau1 extends Phaser.Scene{
             switch (kevent.keyCode)
             {
                 case Phaser.Input.Keyboard.KeyCodes.RIGHT:
-                    me.speed=-1;
+                    me.speed=1;
                     break;
                 case Phaser.Input.Keyboard.KeyCodes.LEFT:
-                    me.speed=1;
+                    me.speed=-1;
                     break;
             }
         });
